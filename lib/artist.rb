bfa55e5a700ea3d1b420d6a675f767900f15a6fd
Heshie Brody <4hbrody@gmail.com>
2018-09-03 01:10:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,7 @@ class Artist
     @@all << self
   end
   def new_song(name, genre)
-    song = Song.new(name, self, genre)
+    Song.new(name, self, genre)
   end
   def songs
     Song.all.select {|s| s.artist == self}
