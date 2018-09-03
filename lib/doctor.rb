3a4c4560e5971e620b2ee889bd2c87cd8bf416bb
Heshie Brody <4hbrody@gmail.com>
2018-09-03 01:33:40 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,8 +6,8 @@ class Doctor
     @name = name
     @@all << self
   end
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
   end
   def appointments
 
