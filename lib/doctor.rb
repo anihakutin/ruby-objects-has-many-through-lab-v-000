1069c62c2838193610d9f557b2695818ee20a3e9
Heshie Brody <4hbrody@gmail.com>
2018-09-03 01:18:39 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,7 +3,21 @@ class Doctor
   attr_accessor :name
 
   def initialize(name)
+    @name = name
+  end
+  def new_appointment
+
+  end
+  def appointments
     
+  end
+  def patients
+
+  end 
+
+# Class methods
+  def self.all
+    @@all
   end
 
 end
